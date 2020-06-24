e1a00fee00a94263230d3ab5fc83a7c24837f07a
Shin-sae-byeok <toqur1219@naver.com>
2020-06-25 01:00:20 +0900

    feat:android title view dev

--- Test_Dev/app/src/main/java/com/callblackbox/MainActivity.kt
+++ Test_Dev/app/src/main/java/com/callblackbox/MainActivity.kt
@@ -2,10 +2,23 @@ package com.callblackbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) { //앱이 최초 실행했을 때 수행
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //xml 화면 뷰를 연결
+
+
+        btn_get.setOnClickListener{
+            var resultText = et_id.text.toString()
+            tv_title.setText(resultText)
+
+        }
+        tv_title.setText("Hello World..")
+
+
     }
 }
