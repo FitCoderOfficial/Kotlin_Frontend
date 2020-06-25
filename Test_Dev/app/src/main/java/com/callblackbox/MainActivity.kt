d9d7748ae5267578f52f9dbec8a05bdda6571d5c
Shin-sae-byeok <toqur1219@naver.com>
2020-06-26 00:06:23 +0900

    feat:view chang test2

--- Test_Dev/app/src/main/java/com/callblackbox/MainActivity.kt
+++ Test_Dev/app/src/main/java/com/callblackbox/MainActivity.kt
@@ -23,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         btn_sub.setOnClickListener {
             // var는 변수를 의미 val 자바에서 final을 의미, 변경되지 못하는 변수
             val intent = Intent(this, Subactivity::class.java) // 다음 화면으로 이동하기 위한 인텐트 객체
+
+            intent.putExtra( "msg", msg_sending.text.toString()) //타이틀 텍스트를 msg라는 키값으로 묶음
             startActivity(intent)
 
         }
--- Test_Dev/app/src/main/java/com/callblackbox/Subactivity.kt
+++ Test_Dev/app/src/main/java/com/callblackbox/Subactivity.kt
@@ -3,12 +3,18 @@ package com.callblackbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_subactivity.*
 
 class Subactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        if(intent.hasExtra("msg"))
+        {
 
+            tv_getmsg.text = intent.getStringExtra( "msg")
+
+        }
 
 
 
