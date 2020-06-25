fdfcfdb3eea063b561c99dc93bc8e2ee3a8f35a2
Shin-sae-byeok <toqur1219@naver.com>
2020-06-25 23:10:01 +0900

    feat:view chang test

--- Test_Dev/app/src/main/java/com/callblackbox/MainActivity.kt
+++ Test_Dev/app/src/main/java/com/callblackbox/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.callblackbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,11 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main) //xml 화면 뷰를 연결
 
 
-        btn_get.setOnClickListener{
+        btn_get.setOnClickListener {
             var resultText = et_id.text.toString()
-            tv_title.setText(resultText)
+            tv_title.setText(resultText + "님 반갑습니다.")
 
         }
+
+        btn_sub.setOnClickListener {
+            // var는 변수를 의미 val 자바에서 final을 의미, 변경되지 못하는 변수
+            val intent = Intent(this, Subactivity::class.java) // 다음 화면으로 이동하기 위한 인텐트 객체
+            startActivity(intent)
+
+        }
+
         tv_title.setText("Hello World..")
 
 
