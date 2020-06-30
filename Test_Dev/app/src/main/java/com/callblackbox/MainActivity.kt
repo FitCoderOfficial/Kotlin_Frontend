aa31e654c66df2450c88ace58a3e3677d82a9249
Shin-sae-byeok <toqur1219@naver.com>
2020-07-01 00:03:49 +0900

    refactor:register and login

--- Test_Dev/app/src/main/java/com/callblackbox/MainActivity.kt
+++ Test_Dev/app/src/main/java/com/callblackbox/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.callblackbox
 
-import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
@@ -13,27 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //xml 화면 뷰를 연결
 
-
-        btn_get.setOnClickListener {
-            var resultText = et_id.text.toString()
-            tv_title.setText(resultText + "님 반갑습니다.")
-
-        }
-
-        btn_sub.setOnClickListener {
-            // var는 변수를 의미 val 자바에서 final을 의미, 변경되지 못하는 변수
-            val intent = Intent(this, Subactivity::class.java) // 다음 화면으로 이동하기 위한 인텐트 객체
-
-            //intent.putExtra( "msg", msg_sending.text.toString()) //타이틀 텍스트를 msg라는 키값으로 묶음
-            startActivity(intent)
-            finish()
-
-        }
-
-        btn_sub2.setOnClickListener {
-            val intent2 = Intent(this, ImageActivity::class.java) // 다음 화면으로 이동하기 위한 인텐트 객체
-            startActivity(intent2)
-        }
 
 
 
