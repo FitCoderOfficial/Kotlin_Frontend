dd77ca1106936299ea1657b995884ab6d32ef448
Shin-sae-byeok <toqur1219@naver.com>
2020-07-04 16:08:37 +0900

    feat:dependency and adding intent

--- Test_Dev/app/src/main/java/com/callblackbox/LoginActivity.kt
+++ Test_Dev/app/src/main/java/com/callblackbox/LoginActivity.kt
@@ -2,13 +2,18 @@ package com.callblackbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.content.Intent
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
